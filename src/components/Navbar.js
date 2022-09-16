7a7c41a8eb7396fb30ea76846230983c911be512
Brandon Armand <brandonarmand96@gmail.com>
2022-09-16 13:19:02 +0100

    Quiz application for kwangas

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+        <div className="logo">LeRoi Quiz</div>
+            
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/components/QuizScreen.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import QuestionList from "../data/questions.json";
+import Question from './Question';
+import QuizResult from './QuizResult';
+
+function QuizScreen({retry}) {
+    const [currentQuestionIndex, setCurrentQuestionIndex]  = useState(0);
+    const [markedAnswers, setMarkedAnswers] = useState(new Array(QuestionList.length));
+    const isQuestionEnd = currentQuestionIndex === QuestionList.length;
+
+    function calcuateResult() {
+        let correct = 0;
+        QuestionList.forEach((question, index) => {
+            if (question.correctOptionIndex === markedAnswers[index]){
+                correct ++;
+            }
+        });
+        return {
+            total: QuestionList.length-1,
+            correct: correct,
+            percentage: Math.trunc((correct / (QuestionList.length-1)) * 100)
+        };
+    }
+
+    return (
+        <div className="quiz-screen">
+            {
+                isQuestionEnd ? (
+                    <QuizResult
+                    result = {calcuateResult()}
+                    retry = {retry}
+                     />
+                ) : (
+                    <Question
+                        question ={QuestionList[currentQuestionIndex]}
+                        totalQuestions={QuestionList.length-1}
+                        currentQuestion = {currentQuestionIndex}
+                        setAnswer = {(index) => {
+                            setMarkedAnswers((arr) => {
+                                let newArr = [...arr];
+                                newArr[currentQuestionIndex] = index;
+                                return newArr;
+
+                            });
+                            setCurrentQuestionIndex(currentQuestionIndex + 1);
+                        }}
+                    />
+                )
+            }
+            
+        </div>
+    );
+}
+
+export default QuizScreen;
+
